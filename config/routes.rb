64d4e18cf385fbee873b271c2d9fc47e5026d7a0
Aaron Jacobowitz <amjacobowitz@gmail.com>
2016-11-19 17:33:53 -0500

    remove hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'small_groups#index'
+  resources :teachers do
+    resources :klasses do
+      resources :groups do
+        resoures :students
+      end
+      resources :students do
+        resources :goals
+      end
+    end
+  end
 end
