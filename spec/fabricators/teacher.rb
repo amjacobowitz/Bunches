72c85964b93dbe664672bb085fbfe2507c97b67e
Aaron Jacobowitz <amjacobowitz@gmail.com>
2017-01-19 23:00:49 -0500

    change seed

--- spec/fabricators/teacher.rb
+++ spec/fabricators/teacher.rb
@@ -1,6 +1,5 @@
 Fabricator(:teacher) do
-  first_name FFaker::Name.first_name
-  last_name FFaker::Name.last_name
+  name FFaker::Name.first_name
   password 'password'
   email FFaker::Internet.email
 end
