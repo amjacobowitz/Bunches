0fb624072abfb27921ab7bab4be6b4400087b744
Aaron Jacobowitz <amjacobowitz@gmail.com>
2017-04-02 13:19:49 -0400

    lower case

--- client/app/bundles/SmallGroups/routes/student/landing.js
+++ client/app/bundles/SmallGroups/routes/student/landing.js
@@ -45,7 +45,7 @@ class Landing extends Component {
           style={ styles.input }
         />
         <Button
-          text="Get Started"
+          text="get started"
           disabled={ !this.state.name || !this.state.pin }
           onClick={ this.onClick }
         />
